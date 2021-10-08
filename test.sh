b7e8d1a9d5d8a44b9bd81aa2575358ac433ffe2c
Ammar Yasir Naich <ammar.naich@gmail.com>
2021-10-08 19:08:05 +0000

    learning curve produce, parameters changed in train and test scripts

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-python test.py --tag default --resumed_model kitti_Car_best.pth.tar
+# python test.py --tag default --resumed_model kitti_Car_best.pth.tar
+
+
+
+python test.py --batch_size 4  --workers 12 --tag pre_trained_car  --resumed_model kitti_Car_best.pth.tar
+
 
 
 
